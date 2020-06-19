dc18f8f0749d7799372d2695d86d928a737fc569
nikhildhruwe <nick.dhruwe@gmail.com>
2020-06-19 23:44:45 +0530

    Modified, Happy and Sad test case

--- src/test/java/com/moodanalysertesting/MoodAnalyserTest.java
+++ src/test/java/com/moodanalysertesting/MoodAnalyserTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-
     @Test
     public void givenMood_WhenSad_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
