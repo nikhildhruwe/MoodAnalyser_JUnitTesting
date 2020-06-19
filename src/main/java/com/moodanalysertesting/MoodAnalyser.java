d10b1c89366a538866784a9f879060290e9ab784
nikhildhruwe <nick.dhruwe@gmail.com>
2020-06-19 23:43:35 +0530

    Initial commit, Happy and Sad message test

---
+++ src/main/java/com/moodanalysertesting/MoodAnalyser.java
@@ -0,0 +1,11 @@
+package com.moodanalysertesting;
+
+public class MoodAnalyser {
+    public String analyseMood(String message) {
+        if(message.contains("sad"))
+            return "SAD";
+        else {
+            return "HAPPY";
+        }
+    }
+}
---
+++ src/test/java/com/moodanalysertesting/MoodAnalyserTest.java
@@ -0,0 +1,21 @@
+package com.moodanalysertesting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+
+    @Test
+    public void givenMood_WhenSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in sad mood.");
+        Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void givenMood_WhenHappy_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in happy mood.");
+        Assert.assertEquals("HAPPY",mood);
+    }
+}
