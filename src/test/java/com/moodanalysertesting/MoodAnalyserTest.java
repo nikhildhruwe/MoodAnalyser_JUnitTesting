69b4d9ddfe69d54ed25e42e1a1160bfd1acff742
nikhildhruwe <nick.dhruwe@gmail.com>
2020-06-25 12:24:27 +0530

    Added-test cases for default constructor

--- src/test/java/com/moodanalysertesting/MoodAnalyserTest.java
+++ src/test/java/com/moodanalysertesting/MoodAnalyserTest.java
@@ -8,39 +8,38 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class MoodAnalyserTest {
+    //Default constructor
+    @Test
+    public void givenMoodAnalyserClass_UsedDefaultConstructor_WhenProper_ShouldReturnObject() throws
+            MoodAnalyserException {
+        MoodAnalyser moodAnalyser = MoodAnalyserReflector.createMoodAnalyser();
+        try {
+            String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        }catch (MoodAnalyserException e){
+            e.printStackTrace();
+        }
+    }
 
-  /*  @Test
-    public void givenMoodAnalyserClass_UsedDefaultConstructor_WhenProper_ShouldReturnObject()
-            throws MoodAnalyserException {
-        MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyser(null
-                , "com.moodanalysertesting.MoodAnalyser");
-        Assert.assertEquals(true, equals(moodAnalyser));
+    @Test
+    public void givenMoodAnalyserClass_UsedDefaultConstructor_WhenImproper_ThrowException(){
+        try {
+            MoodAnalyser moodAnalyser = MoodAnalyserReflector.createMoodAnalyser();
+        }catch (MoodAnalyserException e){
+            Assert.assertEquals("Entered Wrong Class",e.getMessage());
+        }
     }
 
 
     @Test
-    public void givenClassName_WhenImproper_ForDefaultConstructor_ShouldThrowException()
-    throws MoodAnalyserException {
-        try {
-            MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyser(null,
-                    "com.moodanalysertesting.MoodAnalysera");
-        } catch (Exception e) {
-            Assert.assertEquals("Entered Wrong class name", e.getMessage());
+    public void givenMoodAnalyserConstructor_UsedDefaultConstructor_WhenImproper_ThrowException() {
+        try{
+            MoodAnalyser moodAnalyser = MoodAnalyserReflector.createMoodAnalyser();
+            moodAnalyser.analyseMood();
+        }catch (MoodAnalyserException e){
+            e.printStackTrace();
         }
     }
-
-   */
-
-    /*@Test
-    public void givenHappyMessage_WhenProper_ShouldReturnHappyMood() {
-        try {
-            MoodAnalyser moodAnalyser = MoodAnalyserReflector.createMoodAnalyser
-                    ("I am in a Happy mood","com.moodanalysertesting.MoodAnalyser");
-            Assert.assertEquals("HAPPY",MoodAnalyserReflector.createMoodAnalyser(moodAnalyser));
-        } catch (NoSuchMethodException e) {
-            System.out.println(e.getMessage());
-    }*/
-    //Default constructor
 
     //Parameterised constructor
     @Test
